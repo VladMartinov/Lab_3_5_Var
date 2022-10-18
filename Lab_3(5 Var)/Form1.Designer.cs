22b14f67f6516ddba165002ed2f4b886b2b30392
VladMartinov <vladmartinov1704@mail.ru>
2022-10-18 10:50:24 +0300

    Добавьте файлы проекта.

---
+++ Lab_3(5 Var)/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Lab_3_5_Var_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonWrite = new System.Windows.Forms.Button();
+            this.buttonDisplay = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 78.16901F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 21.83099F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1158, 679);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1158, 530);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.tableLayoutPanel2.ColumnCount = 3;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.3F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.4F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.3F));
+            this.tableLayoutPanel2.Controls.Add(this.buttonClear, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.buttonDisplay, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.buttonWrite, 0, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(192, 574);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(15);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(773, 90);
+            this.tableLayoutPanel2.TabIndex = 1;
+            // 
+            // buttonWrite
+            // 
+            this.buttonWrite.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonWrite.BackColor = System.Drawing.SystemColors.Info;
+            this.buttonWrite.Font = new System.Drawing.Font("Trebuchet MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonWrite.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.buttonWrite.Location = new System.Drawing.Point(107, 55);
+            this.buttonWrite.Margin = new System.Windows.Forms.Padding(0);
+            this.buttonWrite.MinimumSize = new System.Drawing.Size(150, 35);
+            this.buttonWrite.Name = "buttonWrite";
+            this.buttonWrite.Size = new System.Drawing.Size(150, 35);
+            this.buttonWrite.TabIndex = 0;
+            this.buttonWrite.Text = "Запись в файл";
+            this.buttonWrite.UseVisualStyleBackColor = false;
+            this.buttonWrite.Click += new System.EventHandler(this.buttonWrite_Click);
+            // 
+            // buttonDisplay
+            // 
+            this.buttonDisplay.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonDisplay.BackColor = System.Drawing.SystemColors.Info;
+            this.buttonDisplay.Font = new System.Drawing.Font("Trebuchet MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonDisplay.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.buttonDisplay.Location = new System.Drawing.Point(306, 55);
+            this.buttonDisplay.Margin = new System.Windows.Forms.Padding(49, 0, 50, 0);
+            this.buttonDisplay.MinimumSize = new System.Drawing.Size(150, 35);
+            this.buttonDisplay.Name = "buttonDisplay";
+            this.buttonDisplay.Size = new System.Drawing.Size(159, 35);
+            this.buttonDisplay.TabIndex = 1;
+            this.buttonDisplay.Text = "Отображение";
+            this.buttonDisplay.UseVisualStyleBackColor = false;
+            this.buttonDisplay.Click += new System.EventHandler(this.buttonDisplay_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonClear.BackColor = System.Drawing.SystemColors.Info;
+            this.buttonClear.Font = new System.Drawing.Font("Trebuchet MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonClear.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.buttonClear.Location = new System.Drawing.Point(515, 55);
+            this.buttonClear.Margin = new System.Windows.Forms.Padding(0);
+            this.buttonClear.MinimumSize = new System.Drawing.Size(150, 35);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(150, 35);
+            this.buttonClear.TabIndex = 2;
+            this.buttonClear.Text = "Очистка";
+            this.buttonClear.UseVisualStyleBackColor = false;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.NavajoWhite;
+            this.ClientSize = new System.Drawing.Size(1182, 703);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MinimumSize = new System.Drawing.Size(1200, 750);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonDisplay;
+        private System.Windows.Forms.Button buttonWrite;
+    }
+}
+
---
+++ Lab_3(5 Var)/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Lab_3_5_Var_
+{
+    public partial class Form1 : Form
+    {
+        string path = "C:\\TestFile.txt";
+
+        string[] arrayLine = new string[10];
+
+        string[] readyArrayLine = {
+                    "First line",
+                    "Second line",
+                    "Third line",
+                    "Fourth line",
+                    "Fifth line",
+                    "Sixth line",
+                    "Seventh line",
+                    "Eighth line",
+                    "Ninth line",
+                    "Tenth line"
+                };
+
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.BackColor = Color.FromKnownColor(KnownColor.Azure);
+
+            if (File.Exists(path))
+            {
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    string line;
+                    int i = 0;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        arrayLine[i] = line;
+                        i++;
+                    }
+                }
+            }
+        }
+
+        private void buttonWrite_Click(object sender, EventArgs e)
+        {
+            using (StreamWriter writer = new StreamWriter(path, false))
+            {
+                for (int i = 0; i < arrayLine.Length; i++)
+                {
+                    writer.WriteLine(arrayLine[i]);
+                }
+            }
+
+        }
+
+        private void buttonDisplay_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < arrayLine.Length; i++)
+                if (arrayLine[i] == null)
+                    arrayLine[i] = readyArrayLine[i];
+            
+            Graphics g = pictureBox1.CreateGraphics();
+
+            FontStyle fontStyle;
+            Font fn = new Font("Arial", 36); 
+            StringFormat sf;
+
+            float marginTop = 0;
+
+            for (int i = 0; i < arrayLine.Length; i++)
+            {
+                if (i < 6)
+                {
+                    sf = (StringFormat)StringFormat.GenericTypographic.Clone();
+
+                    fontStyle = FontStyle.Underline;
+
+                    fn = new Font("Arial", 36, fontStyle);
+
+                    sf.Alignment = StringAlignment.Center;
+                    sf.LineAlignment = StringAlignment.Center;
+                    sf.FormatFlags = StringFormatFlags.DirectionVertical;
+
+                    g.DrawString(arrayLine[i], fn, Brushes.Black,
+                                 new RectangleF(0 + marginTop, 0, this.pictureBox1.Width,
+                                 this.pictureBox1.Height), sf);
+
+                    marginTop += 48;
+                }
+                else if (i > 5 && i < 9)
+                {
+                    if(i == 6)
+                        marginTop = 0;
+
+                    sf = (StringFormat)StringFormat.GenericTypographic.Clone();
+
+                    fontStyle = FontStyle.Regular;
+
+                    fn = new Font("Broadway", 24, fontStyle);
+
+                    sf.Alignment = StringAlignment.Far;
+                    sf.LineAlignment = StringAlignment.Near;
+
+                    g.DrawString(arrayLine[i], fn, Brushes.Black,
+                                 new RectangleF(0, 0 + marginTop, this.pictureBox1.Width,
+                                this.pictureBox1.Height), sf);
+
+                    marginTop += 32;
+                } else if( i > 8)
+                {
+                    g.PageUnit = GraphicsUnit.Inch;
+
+                    sf = (StringFormat)StringFormat.GenericTypographic.Clone();
+
+                    fontStyle = FontStyle.Strikeout;
+
+                    fn = new Font("Times New Roman", 96 * 0.5f, fontStyle);
+
+                    sf.Alignment = StringAlignment.Near;
+                    sf.LineAlignment = StringAlignment.Far;
+
+                    g.DrawString(arrayLine[i], fn, Brushes.Black,
+                                 new RectangleF(0, 0, this.pictureBox1.Width/96,
+                                 this.pictureBox1.Height/96), sf);
+                }
+            }
+
+            fn.Dispose();
+        }
+        
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+            g.Clear(pictureBox1.BackColor);
+        }
+
+    }
+}
